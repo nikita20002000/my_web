293583c1e50a724cdef1ef15a338ae50c7808458
nikita20002000 <vertihost@yandex.com>
2023-06-30 19:32:36 +0300

    one_more_commit

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about_me'),
+    path('contacts', views.contacts, name='contact_page'),
+    path('pixel_town', views.pixel_town, name='pixel_town'),
+    path('help', views.help_me, name='help_me')
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
 
-
 def index(request):
     return render(request, 'main/index.html', {'title': 'Добро пожаловать!'})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
+def pixel_town(request):
+    return render(request, 'main/pixel_town.html')
+
+
+def help_me(request):
+    return render(request, 'main/help.html')
